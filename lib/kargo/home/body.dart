4dc284ddd359019f7abf10823482b29c9e3357bb
sedakaraatas <sedakaraatas5@gmail.com>
2022-12-07 18:57:57 +0300

    add eksik kargo home

--- lib/kargo/home/body.dart
+++ lib/kargo/home/body.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+
 class KargoBody extends StatelessWidget {
   const KargoBody({super.key});
 
@@ -7,9 +8,12 @@ class KargoBody extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
+        
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
+          
           children: [
+      
             Flexible(
               child: Container(
                 height: MediaQuery.of(context).size.height*0.3,
@@ -21,17 +25,62 @@ class KargoBody extends StatelessWidget {
                     image:AssetImage("assets/kargoslogan.png"),
                   ),
                 ),
-                child: RichText(text: 
-                TextSpan(
-                  style: TextStyle(background:Paint(
-                  ) )
-                )),
+              
               )
               ,
+              
         ),
+        SizedBox(
+          height: MediaQuery.of(context).size.height/10,
+        ),
+        Padding(
+          padding: const EdgeInsets.only(right: 20, left: 20),
+          child: Container(
+            alignment:Alignment.center,
+            child: DecoratedBox(
+              decoration: BoxDecoration(
+                color: Colors.white,
+                boxShadow: [
+            BoxShadow(color: Colors.grey.withOpacity(0.5),
+          spreadRadius: 5,
+          blurRadius: 7,
+          offset: const Offset(3, 3),
+          )
+          ]
+              ),
+    
+  child: const Padding(
+    padding: EdgeInsets.only(right:10.0, left: 20, bottom: 30),
+    child: Text.rich(TextSpan(
+      text: ' \n\nNasıl Kargo Eklenir?  \n\n',
+      style: TextStyle(
+        fontSize: 25,fontWeight: FontWeight.bold,
+        decoration: TextDecoration.underline),
+        children: <TextSpan>[
+        TextSpan(
+              text: '•Kargo ekle tuşuna basınız.\n\n•Kargonuzun ait olduğu firma adını seçiniz.\n\n•Sipariş numaranızı giriniz.\n\n•Kargomu getir tuşuna basarak kaydınızı oluşturunuz.\n',
+              style: TextStyle(fontSize: 20,fontWeight: FontWeight.normal,
+        decoration: TextDecoration.none
+              )),
+        // can add more TextSpans here...
       ],
+        ),),
+  ),
+
+      
+            )
+      
+          ),
+          
         ),
         
-      ),);  
+          ],
+          
+      ),
+      
+      )
+      
+      );
+        
   }
 }
--- lib/kargo/home/kargohome.dart
+++ lib/kargo/home/kargohome.dart
@@ -8,7 +8,7 @@ class KargoHomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return const Scaffold(
       backgroundColor: kbackgroundColor,
       bottomNavigationBar: BottomNavBar(),
       body:KargoBody() ,
