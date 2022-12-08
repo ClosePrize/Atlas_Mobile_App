f81a662e5d266dadaf94f2915cf8afd80c0d744d
sedakaraatas <sedakaraatas5@gmail.com>
2022-12-08 18:01:30 +0300

    navbar updated

--- lib/kargo/cargo_add/cargo_add.dart
+++ lib/kargo/cargo_add/cargo_add.dart
@@ -9,6 +9,7 @@ class KargoHomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
+      
       backgroundColor: kbackgroundColor,
       bottomNavigationBar: BottomNavBar(),
       body:KargoAdd() ,
--- lib/kargo/widgets/reusable_widgets.dart
+++ lib/kargo/widgets/reusable_widgets.dart
@@ -181,4 +181,3 @@ class addButton extends StatelessWidget {
         );
   }
 }
-
