5cb1487857958d7a85372c643cfef4472fdfc938
sedakaraatas <sedakaraatas5@gmail.com>
2022-12-08 18:01:30 +0300

    navbar updated

--- lib/kargo/cargo_add/body.dart
+++ lib/kargo/cargo_add/body.dart
@@ -1,11 +1,20 @@
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/material.dart';
+import 'package:v01/kargo/constants.dart';
+import 'package:v01/kargo/widgets/bottomnavigationbar.dart';
 
 class KargoAdd extends StatelessWidget {
   const KargoAdd({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      extendBodyBehindAppBar: true,
+      appBar: AppBar(
+        backgroundColor: const Color.fromARGB(255, 2, 12, 36),
+        elevation: 2,
+      ),
+      bottomNavigationBar: const BottomNavBar(),
+      backgroundColor: kbackgroundColor,
+    );
   }
 }
