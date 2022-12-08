1e3e5780674043c7c816a13d727090079b85a5e8
sedakaraatas <sedakaraatas5@gmail.com>
2022-12-08 18:34:35 +0300

    kargo add added

--- lib/kargo/cargo_add/cargo_add.dart
+++ lib/kargo/cargo_add/cargo_add.dart
@@ -3,8 +3,8 @@ import 'package:v01/kargo/cargo_add/body.dart';
 import 'package:v01/kargo/constants.dart';
 import 'package:v01/kargo/widgets/bottomnavigationbar.dart';
 
-class KargoHomeScreen extends StatelessWidget {
-  const KargoHomeScreen({super.key});
+class KargoAddScreen extends StatelessWidget {
+  const KargoAddScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
