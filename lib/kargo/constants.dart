589ec920a2db86d9e111687d0bd3e1bdf88134db
sedakaraatas <sedakaraatas5@gmail.com>
2023-01-20 21:19:55 +0300

    colors updated

--- lib/kargo/constants.dart
+++ lib/kargo/constants.dart
@@ -7,7 +7,7 @@ const kargogreenColor = Color.fromARGB(255, 69, 197, 37);
 const kargoredColor = Color.fromARGB(255, 220, 12, 21);
 const kargoorangeColor = Color.fromARGB(255, 255, 168, 0);
 
-const kitPrimaryColor = Color.fromARGB(255, 128, 4, 4);
+const kitPrimaryColor = Color.fromARGB(255, 227, 50, 23);
 //const kitPrimaryColor1 = Color.fromARGB(255, 220, 12, 21);
 const kitgreenColor = Color.fromARGB(255, 88, 180, 84);
 const kitblueColor = Color.fromARGB(255, 46, 41, 78);
