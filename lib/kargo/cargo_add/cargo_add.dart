36ff8ac2ea717987ad45fe3b0b4dd6b57474e66a
ClosePrize <circimustafaa@gmail.com>
2023-01-23 21:32:40 +0300

    Kargo Add için Floating Action Button düzenlemesi

--- lib/kargo/cargo_add/cargo_add.dart
+++ lib/kargo/cargo_add/cargo_add.dart
@@ -49,18 +49,6 @@ children: [
               )
               ),
 ],  ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {  Navigator.pushReplacement<void, void>(
-          context,
-          MaterialPageRoute<void>(
-            builder: (BuildContext context) =>  KitHomePage(),
-          ),
-        );  },
-        child: ClipRRect(child:Image.asset('assets/kitlogo1.jpg',
-
-          fit: BoxFit.fitHeight,height: 120,),borderRadius: BorderRadius.circular(40),),
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.endDocked,
     );
   }
 }
