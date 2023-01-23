69c6a2632d0ebf3de16af136e06c81f0ef68531e
sedakaraatas <sedakaraatas5@gmail.com>
2023-01-23 17:00:38 +0300

    font

--- lib/kargo/cargo_add/cargo_add.dart
+++ lib/kargo/cargo_add/cargo_add.dart
@@ -4,6 +4,8 @@ import 'package:v01/kargo/constants.dart';
 import 'package:v01/kargo/widgets/bottomnavigationbar.dart';
 import 'package:v01/kargo/widgets/reusable_widgets.dart';
 
+import '../../kit/home/home.dart';
+
 class KargoAddScreen extends StatelessWidget {
     TextEditingController _siparisTextController = TextEditingController();
 
@@ -23,6 +25,18 @@ class KargoAddScreen extends StatelessWidget {
       ),
       backgroundColor: kbackgroundColor,
       bottomNavigationBar: CargoAddNavBar(),
+       floatingActionButton: FloatingActionButton(
+          onPressed: () {  Navigator.pushReplacement<void, void>(
+              context,
+              MaterialPageRoute<void>(
+                builder: (BuildContext context) =>  KitHomePage(),
+              ),
+            );  },
+          child: ClipRRect(child:Image.asset('assets/kitlogo1.jpg',
+         
+            fit: BoxFit.fitHeight,height: 120,),borderRadius: BorderRadius.circular(40),),
+),
+floatingActionButtonLocation: FloatingActionButtonLocation.endDocked, 
       body:
      
           Column(
